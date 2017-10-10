a5d2bb8425a0474ca516d63ac0a4be3539e7c25e
ElenaReach <elenareach@gmail.com>
2017-10-10 05:47:08 -0600

    Angular4 gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { AlertModule } from 'ngx-bootstrap/';
+import { RouterModule } from '@angular/router';
 
+import { ImageFilterPipe } from './image-detail/shared/filter.pipe';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ImageService } from './image-detail/shared/image.service';
+import { ImageDetailComponent } from './image-detail/image-detail.component';
+import {appRoutes} from './routes'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GalleryComponent,
+    ImageDetailComponent,
+    ImageFilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpModule,AlertModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [ImageService,ImageFilterPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
